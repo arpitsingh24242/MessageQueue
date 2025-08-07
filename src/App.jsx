0a661cbe550adf739c6e906732607dabcf17dcfa
Arpit Singh <arpitrkt024@gmail.com>
2025-08-07 12:37:50 +0530

    Fix: ensure app.css is tracked

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import axios from "axios";
-import "./app.css";
+import "./App.css";
 
 export default function App() {
   const [id, setId] = useState("");
